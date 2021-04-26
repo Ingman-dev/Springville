1418aa2c741576f2ade0627915598f0f44d7eeb9
Ingman-dev <morten.ingman@ingmandev.se>
2021-04-26 14:54:50 +0200

    Removed authentication temporarily

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 // in src/App.js
 import * as React from "react";
 import { AdminContext } from "react-admin";
-import { dataProvider, authProvider } from "./client";
+import { dataProvider } from "./client";
 import { Resources } from "./resources";
 
 const App = () => (
-  <AdminContext dataProvider={dataProvider} authProvider={authProvider}>
+  <AdminContext dataProvider={dataProvider} >
     <Resources />
   </AdminContext>
 );
 export default App;
+// authProvider, authProvider={authProvider}
