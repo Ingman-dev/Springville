b313bd87bd900966cd5622c409d06f9a5d6ae8f2
Ingman-dev <morten.ingman@ingmandev.se>
2021-04-26 14:11:04 +0200

    Changes

--- src/client/restClient.js
+++ src/client/restClient.js
@@ -3,7 +3,7 @@ import fAuthClient from "@feathersjs/authentication-client";
 const fRest = require("@feathersjs/rest-client");
 
 const app = feathers();
-const fRestClient = fRest("http://localhost:8081");
+const fRestClient = fRest("https://int.bytapension.se/api/v1/forms-register");
 app.configure(fRestClient.fetch(window.fetch));
 app.configure(fAuthClient({ storage: window.localStorage, timeout: 800000 }));
 
